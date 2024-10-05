789eb1d30d41621da16f0a8fcc856fd16674f263
kjaks <karolisjakas04@gmail.com>
2024-10-05 22:14:11 +0200

    first commit

---
+++ Backend/src/main/java/com/git_bank/demo/GitBankApplication.java
@@ -0,0 +1,13 @@
+package com.git_bank.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBankApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/git_bank/demo/GitBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_bank.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
